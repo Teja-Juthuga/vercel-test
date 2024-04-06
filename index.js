928049d32fb02fbecb028649e5c899dc4a4914b6
Teja Juthuga <tejajuthuga@gmail.com>
2024-04-07 05:16:03 +0530

    CODE UPDATED

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route for the root path
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
